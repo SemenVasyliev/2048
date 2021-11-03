0c97d3c558aa31efcdeccd5ef34e827b5a0d9dd3
felixpincher <felixpincher@gmail.com>
2021-11-03 16:26:22 +0200

    create game mechanics

--- Block.cs
+++ Block.cs
@@ -9,8 +9,9 @@ namespace _2048.DCT
 {
     class Block
     {
-        int num = 0;
-
+        public int num = 0;
+        public bool NewBlock = false;
+        public bool Combined = false;
         public enum Direction { Up = 1, Down = 2, Left = 3, Right = 4 }
 
         private static readonly SolidColorBrush Tile2 = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ebe6df"));
@@ -61,9 +62,298 @@ namespace _2048.DCT
             }
         }
 
-        public static SolidColorBrush GetNoneTitleBlocksColor(Block blk)
+        public static SolidColorBrush GetNoneTitleBlocksColor()
         {
             return NoneTitle;
+        }
+
+        public static void GenerateABlock(ref Block[,] blks)
+        {
+            int k = Block.CountBlocksNumberZero(blks);
+            var ran = new Random();
+            int RanPos = ran.Next(0, k);
+            int RanNum = ran.Next(1, 2) * 2;
+
+            int temp_count = -1;
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    if (blks[row, col].num == 0)
+                    {
+                        temp_count++;
+                        if (temp_count == RanPos)
+                        {
+                            blks[row, col].num = RanNum;
+                            blks[row, col].NewBlock = true;
+                        }
+                    }
+                }
+            }
+        }
+        private static int CountBlocksNumberZero(Block[,] blks)
+        {
+            int kq = 0;
+            for (int row = 0; row < 4; row++)
+                for (int col = 0; col < 4; col++)
+                    if (blks[row, col].num == 0)
+                        kq++;
+            return kq;
+        }
+        public static void InitNewGameBlocks(ref Block[,] blks)
+        {
+            InitBlocks(ref blks);
+            GenerateABlock(ref blks);
+            GenerateABlock(ref blks);
+        }
+
+        public static void InitBlocks(ref Block[,] blks)
+        {
+            for (int row = 0; row < 4; row++)
+                for (int col = 0; col < 4; col++)
+                    blks[row, col] = new Block();
+        }
+
+        public static void CopyBlock(ref Block[,] DesBlks, ref Block[,] SouBlks)
+        {
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    DesBlks[row, col].num = SouBlks[row, col].num;
+                    DesBlks[row, col].Combined = SouBlks[row, col].Combined;
+                    DesBlks[row, col].NewBlock = SouBlks[row, col].NewBlock;
+                }
+            }
+        }
+
+        private static List<int> ProcessList(ref List<int> ls, ref int score)  
+        {
+            List<int> result = new List<int>();
+            int front = 0, end = 1;
+            while (end <= 3 && front <= 2)
+            {
+                if (ls[front] == 0)
+                {
+                    front++;
+                    end = front + 1;
+                }
+                else if (ls[end] == 0)
+                {
+                    end++;
+                }
+                else
+                {
+                    if (ls[front] == ls[end]) 
+                    {
+                        result.Add(front);
+                        ls[front] = ls[front] * 2;
+                        ls[end] = 0;
+                        score = score + ls[front];
+                        front++;
+                        end = front + 1;
+                    }
+                    else
+                    {
+                        front++;
+                        end = front + 1;
+                    }
+                }
+            }
+            int ZeroIndex = -1;
+            for (int i = 0; i < 4; i++)
+            {
+                if (ls[i] == 0)
+                {
+                    if (ZeroIndex == -1)
+                        ZeroIndex = i;
+                }
+                else
+                {
+                    if (ZeroIndex != -1)
+                    {
+                        if (result.Contains(i))
+                        {
+                            result[result.IndexOf(i)] = ZeroIndex;
+                        }
+                        ls[ZeroIndex] = ls[i];
+                        ls[i] = 0;
+                        ZeroIndex = -1;
+                        i = ZeroIndex + 1;
+                        result.Add(-1);
+                    }
+                }
+            }
+            return result;
+        }
+        public static bool TryUp(ref Block[,] blks, ref Block[,] OldBlocks, ref int score) 
+        {
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            CopyBlock(ref TempBlocks, ref blks);
+
+            bool BlocksChanged = false;
+            for (int col = 0; col < 4; col++)
+            {
+                List<int> ls = new List<int>();
+                for (int row = 0; row < 4; row++)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = ProcessList(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[ChangedList[i], col].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int row = 0; row < 4; row++) 
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                CopyBlock(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
+        }
+        public static bool TryDown(ref Block[,] blks, ref Block[,] OldBlocks, ref int score) 
+        {
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            CopyBlock(ref TempBlocks, ref blks);
+
+            bool BlocksChanged = false;
+            for (int col = 0; col < 4; col++)
+            {
+                List<int> ls = new List<int>();
+                for (int row = 3; row >= 0; row--)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = ProcessList(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[3 - ChangedList[i], col].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int row = 3; row >= 0; row--)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                }
+                ls.Clear();
+            }
+
+            if (BlocksChanged == true)
+            {
+                CopyBlock(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
+        }
+        public static bool TryLeft(ref Block[,] blks, ref Block[,] OldBlocks, ref int score) 
+        {     
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            CopyBlock(ref TempBlocks, ref blks);
+
+            bool BlocksChanged = false;
+            for (int row = 0; row < 4; row++)
+            {
+                List<int> ls = new List<int>();
+                for (int col = 0; col < 4; col++)
+                {
+                    ls.Add(blks[row, col].num);
+  
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = ProcessList(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[row, ChangedList[i]].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int col = 0; col < 4; col++)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                CopyBlock(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
+        }
+        public static bool TryRight(ref Block[,] blks, ref Block[,] OldBlocks, ref int score) 
+        {
+            Block[,] TempBlocks = new Block[4, 4];
+            InitBlocks(ref TempBlocks);
+            CopyBlock(ref TempBlocks, ref blks);
+
+            bool BlocksChanged = false;
+            for (int row = 0; row < 4; row++)
+            {
+                List<int> ls = new List<int>();
+                for (int col = 3; col >= 0; col--)
+                {
+                    ls.Add(blks[row, col].num);
+                    blks[row, col].Combined = false;
+                    blks[row, col].NewBlock = false;
+                }
+                List<int> ChangedList = ProcessList(ref ls, ref score);
+                if (ChangedList.Count != 0) 
+                {
+                    BlocksChanged = true;
+                    int l_ls = ChangedList.Count;
+                    for (int i = 0; i < l_ls; i++)
+                    {
+                        if (ChangedList[i] != -1)
+                            blks[row, 3 - ChangedList[i]].Combined = true;
+                    }
+                    int ls_k = 0;
+                    for (int col = 3; col >= 0; col--)
+                    {
+                        blks[row, col].num = ls[ls_k];
+                        ls_k++;
+                    }
+                    ls.Clear();
+                }
+            }
+
+            if (BlocksChanged == true)
+            {
+                CopyBlock(ref OldBlocks, ref TempBlocks);
+            }
+            return BlocksChanged;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace _2048.DCT
 {
@@ -21,11 +22,167 @@ namespace _2048.DCT
     public partial class MainWindow : Window
     {
         int score;
-        Block[,] blocks = new Block[4, 4];
-        
+        Block[,] blks = new Block[4, 4];
+        Block[,] OldBlks = new Block[4, 4];
+        int PrevScore;
+        Button[,] Btns = new Button[4, 4];
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            //Default Score
+            score = 0;
+            //Create new blocks
+            Block.InitNewGameBlocks(ref blks);
+            Block.InitBlocks(ref OldBlks);
+            Block.CopyBlock(ref OldBlks, ref blks);
+            Score.Text = score.ToString();
+            DrawNewBlock();
+        }
+
+        private void Mgrid_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    MoveUp();
+                    break;
+                case Key.Down:
+                    MoveDown();
+                    break;
+                case Key.Right:
+                    MoveRight();
+                    break;
+                case Key.Left:
+                    MoveLeft();
+                    break;
+                default:
+                    break;
+            }
+            if (GameOver(blks))
+            {
+                MessageBox.Show("Game over! Score: " + score.ToString(), "Notification");
+            }
+        }
+
+        private void MoveLeft()
+        {
+            PrevScore = score;
+            if (Block.TryLeft(ref blks, ref OldBlks, ref score) == true)  
+            {
+                Block.GenerateABlock(ref blks);
+                DrawNewBlock();
+                Score.Text = score.ToString();
+            }
+        }
+
+        private void MoveRight()
+        {
+            PrevScore = score;
+            if (Block.TryRight(ref blks, ref OldBlks, ref score) == true)  
+            {
+                Block.GenerateABlock(ref blks);
+                DrawNewBlock();
+                Score.Text = score.ToString();
+            }
+        }
+
+        private void MoveDown()
+        {
+            PrevScore = score;
+            if (Block.TryDown(ref blks, ref OldBlks, ref score) == true) 
+            {
+                Block.GenerateABlock(ref blks);
+                DrawNewBlock();
+                Score.Text = score.ToString();
+            }
+        }
+
+        private void MoveUp()
+        {
+            PrevScore = score;
+            if (Block.TryUp(ref blks, ref OldBlks, ref score) == true)
+            {
+                Block.GenerateABlock(ref blks);
+                DrawNewBlock();
+                Score.Text = score.ToString();
+            }
+        }
+
+        private void DrawNewBlock()
+        {
+            GridClear();
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    Btns[row, col] = new Button();
+                    Btns[row, col].BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("Gray"));
+                    Btns[row, col].BorderThickness = new Thickness(3);
+                    Btns[row, col].FontStretch = new FontStretch();
+                    if (blks[row, col].num != 0) 
+                    {
+                        Btns[row, col].Background = Block.GetTitleBlocksColor(blks[row, col]);
+                        Btns[row, col].Content = blks[row, col].num.ToString();
+                        Btns[row, col].FontSize = 40;                       
+                    }
+                    else
+                    {
+                        Btns[row, col].Background = Block.GetNoneTitleBlocksColor();
+                    }
+                    Grid.SetColumn(Btns[row, col], col);
+                    Grid.SetRow(Btns[row, col], row);
+                    mgrid.Children.Add(Btns[row, col]);
+                }
+            }
+        }
+        private void GridClear()
+        {
+            for (int i = 0; i < mgrid.Children.Count; i++)
+            {
+                if ((Grid.GetColumn(mgrid.Children[i]) != 4))
+                {
+                    mgrid.Children.Remove(mgrid.Children[i]);
+                }
+            }
+        }
+        private bool GameOver(Block[,] blks)
+        {
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    if (blks[row, col].num == 0)
+                        return false;
+                }
+            }
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (blks[row, col].num == blks[row, col + 1].num)
+                        return false;
+                }
+            }
+            for (int col = 0; col < 4; col++)
+            {
+                for (int row = 0; row < 3; row++)
+                {
+                    if (blks[row + 1, col].num == blks[row, col].num)
+                        return false;
+                }
+            }
+            return true;
+        }
+
     }
+
 }
